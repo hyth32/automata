44aa32a705619d864ab1b6cf5d25344a5d649235
hyth32 <5474n.sage@gmail.com>
2024-04-11 10:24:20 -0700

    added boost library

--- mili-mur-mili/main.cpp
+++ mili-mur-mili/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage:" << argv[0] << "program input output";
+        return 1;
+    }
+
+    std::string program = argv[1];
+    std::string inputFile = argv[2];
+    std::string outputFile = argv[3];
+
+    std::cout << "Программа: " << program;
+    std::cout << "Входной файл: " << inputFile;
+    std::cout << "Выходной файл: " << outputFile;
+
     return 0;
 }
